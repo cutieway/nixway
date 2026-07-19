acc180385e53065b888fa2e9256fe0ae9c7366fc
lexi <173241226+cutieway@users.noreply.github.com>
2026-07-20 00:56:38 +0300

    uwu: automatic system rebuild 2026-07-20T00:56:38+03:00

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -2,12 +2,6 @@
 
 {
   home.packages = with pkgs; [
-    (opencode-desktop.overrideAttrs (old: {
-      preFixup = (old.preFixup or "") + ''
-        gappsWrapperArgs+=(
-          --add-flags "--ozone-platform-hint=auto"
-        )
-      '';
-    }))
+    opencode-desktop
   ];
 }
