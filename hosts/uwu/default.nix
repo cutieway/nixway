c95fdcb9fb1e1d67c200f3caba998872754f627e
lexi <173241226+cutieway@users.noreply.github.com>
2026-08-01 07:13:22 +0300

    uwu: auto rebuild 2026-08-01T07:13:22+03:00

--- hosts/uwu/default.nix
+++ hosts/uwu/default.nix
@@ -18,5 +18,9 @@
   ];
 
   networking.hostName = hostname;
+
+  # fnmode=2: Apple keyboard F-keys act as function keys without holding Fn.
+  boot.extraModprobeConfig = "options hid_apple fnmode=2";
+
   system.stateVersion = "26.05";
 }
